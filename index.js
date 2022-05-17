d19e5f2a5964d81f3687b4ef926d67d49ae8aa79
Victor <vx_zz@hotmail.com>
2022-05-17 13:27:29 -0400

    commit tree 8264bf1e292557aaaeadc3f66e1cf51f119a4f3e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (typeof activity === "undefined") {
+      return 'This Saturday, I want to roller-skate!';
+    }
+    else {
+      return `This Saturday, I want to ${activity}!`
+    }
+  }
+
+  function mondayWork(activity) {
+    if (typeof activity === "undefined") {
+      return 'This Monday, I will go to the office.';
+    }
+    else {
+      return `This Monday, I will ${activity}.`
+    }
+  }
+
+  function wrapAdjective(visual = "*") {
+    return function inner(adjective = "special"){
+        return `You are ${visual}${adjective}${visual}!`
+    }
+  }
+
